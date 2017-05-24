0851c967bbbf108c0b14aa2158f26ae3f74eb280
s0f <316024763@qq.com>
2017-05-24 18:05:30 +0800

    '日常开发进度'

---
+++ src/components/common/User.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div>this is template body</div>
+    </div>
+</template>
+<style type="text/scss" lang="scss">
+</style>
+<script>
+    export default {
+        data () {
+            return {};
+        }
+    };
+</script>
---
+++ src/store/modules/pageMatch.js
@@ -0,0 +1,3 @@
+/**
+ * Created by ssf on 2017/5/19.
+ */
